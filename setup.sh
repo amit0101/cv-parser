82a67182cb67bce7b057ccc58a7ddf86a0b57543
amit0101 <amit.kr.py@gmail.com>
2024-10-24 02:17:58 +0700

    packages update

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 # setup.sh
-apt-get update && apt-get install -y tesseract-ocr poppler-utils libjpeg-dev zlib1g-dev libtiff-dev libfreetype6-dev
+# apt-get update && apt-get install -y tesseract-ocr poppler-utils libjpeg-dev zlib1g-dev libtiff-dev libfreetype6-dev
