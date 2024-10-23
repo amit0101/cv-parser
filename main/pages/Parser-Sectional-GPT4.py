aa7e299b178149dc648e7caa7439bad6534cd727
amit0101 <amit.kr.py@gmail.com>
2024-10-24 03:34:27 +0700

    packages update

--- main/pages/Parser-Sectional-GPT4.py
+++ main/pages/Parser-Sectional-GPT4.py
@@ -95,7 +95,7 @@ def identify_section(text):
         max_tokens=300
     )
 
-    # st.write(f"Response: {response}")
+    st.write(f"Response: {response}")
     section = response.choices[0].message.content
     st.write(f"Identified section for text: {text}\nSection: {section}")
     return section
@@ -121,7 +121,7 @@ def parse_text_with_llm_for_section(text, section, examples):
         max_tokens=1500
     )
 
-    # st.write(f"Response: {response}")
+    st.write(f"Response: {response}")
     response_text = response.choices[0].message.content
     st.info(f"Model Response for section {section}: {response_text}")
 
